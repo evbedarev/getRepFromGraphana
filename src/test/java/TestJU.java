06036821c7269104cf590cfd3c10d9db6a313022
mj <madjo85@yandex.ru>
2019-02-21 16:26:37 +0300

    change settings pom.xml chages for nix

--- src/test/java/TestJU.java
+++ src/test/java/TestJU.java
@@ -18,7 +18,7 @@ public class TestJU {
         List<String> strings = new ArrayList<>();
         strings.add("https://avatars.mds.yandex.net/get-pdb/932587/260efa1d-862e-4b53-937c-c7149df36336/s1200?webp=false");
         strings.add("https://zone-cs.ru/files/forums_imgs/1540997310.png");
-        GetReportFromGraphana getReportFromGraphana = new GetReportFromGraphana(strings, "/home/");
+        GetReportFromGraphana getReportFromGraphana = new GetReportFromGraphana(strings, "/home/", "");
         getReportFromGraphana.savePngToPdf();
 
     }
