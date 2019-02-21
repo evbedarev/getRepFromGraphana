0864f0ae5dc680e3ceeeb4e505273e9dbb3bfa45
madjo <madjo85@yandex.ru>
2019-02-21 10:44:21 +0300

    change getReportFromGraphana add cUrl add HashMap

--- src/main/java/rep/Main.java
+++ src/main/java/rep/Main.java
@@ -8,13 +8,14 @@ import java.util.List;
  * arg -e - end time
  * arg -f -  file to save
  * arg -path - path to file example '/home/mj/'
- * arg -path_to_build '/home/mj/'
+ * arg -path_to_aft '/home/mj/' - path to foldter to save pdf.
  */
 public class Main {
     private static List<Integer> countArgsList = new ArrayList<>();
     private static int argBeginTime = 0;
     private static int argEndTime = 0;
     private static int argPathToSave = 0;
+    private static int argPathToAft = 0;
     private static int countArgs = 0;
 
     public static void main(String[] args) throws Exception {
@@ -36,7 +37,10 @@ public class Main {
                 graphanaUrls,
                 args[argPathToSave + 1]);
         getReportFromGraphana.savePngToPdf();
-//        getReportFromGraphana.downloadFileUsingCurl();
+
+        CopyPdfInArtifacts copyPdfInArtifacts = new CopyPdfInArtifacts();
+        String lastFolder = copyPdfInArtifacts.findNumericDirAndSort(args[argPathToAft + 1]);
+        copyPdfInArtifacts.copyPdfToBuildAndDeleteFromSource(lastFolder);
     }
 
     /*check arguments*/
@@ -48,10 +52,12 @@ public class Main {
                 argEndTime = countArgs;
             if (arg.equals("-path"))
                 argPathToSave = countArgs;
+            if (arg.equals("-path_to_aft"))
+                argPathToAft = countArgs;
             countArgs++;
         }
-        if (argBeginTime == 0 || argEndTime == 0 || argPathToSave == 0)
-            throw new IllegalArgumentException("no parametr -b or -e or -path");
+        if (argBeginTime == 0 || argEndTime == 0 || argPathToSave == 0 || argPathToAft == 0)
+            throw new IllegalArgumentException("no parametr -b or -e or -path or -path_to_aft");
         if (!args[argBeginTime + 1].matches("[0-9]*") || !args[argEndTime + 1].matches("[0-9]*") )
             throw new IllegalArgumentException("Invalid argument, must be numeric");
     }
